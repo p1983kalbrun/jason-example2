d5dc94fb06ef54138daa2636e60968d417f3ec10
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-09-26 11:20:19 +0100

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/ddd/JasonExample2Application.java
@@ -0,0 +1,12 @@
+package ddd;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasonExample2Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(JasonExample2Application.class, args);
+	}
+
+}
